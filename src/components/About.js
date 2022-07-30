2bbf4abc03a8befcb687e7efae01499c91a07d79
user name <user emaile>
2022-07-30 22:04:12 +0300

    portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,7 @@ class About extends Component {
                 <span style={{ cursor: "auto" }}>
                   <img
                     height="250px"
-                    src={profilepic}
+                    src={Fatima-khwaileh-pic}
                     alt="Avatar placeholder"
                   />
                   <Icon
